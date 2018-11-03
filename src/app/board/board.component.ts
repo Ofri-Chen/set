ad5c09f646de6b2543ec26ee743885128fe6d13e
Thankjnvv <ofrichen11@gmail.com>
2018-11-03 17:57:49 +0200

    getAvailableSets

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -16,6 +16,7 @@ export class BoardComponent implements OnInit {
 	deck: Card[] = [];
 	boardCards: Card[] = [];
 	selectedCards: Card[] = [];
+	score: number = 0;
 
 
 	constructor(private cardsService: CardsService,
@@ -58,19 +59,18 @@ export class BoardComponent implements OnInit {
 	}
 
 	handleSuccessfulSet() {
-		alert("success");
 		if (this.deck.length) {
 			this.selectedCards.forEach(setCard => {
 				let newCard: Card = this.deck.splice(0, 1)[0];
 				this.swapCardOnBoard(setCard, newCard);
 			})
 
+			this.score++;
 			this.selectedCards = [];
 		}
 	}
 
 	handleUnsuccessfulSet() {
-		alert("fail");
 		this.selectedCards.forEach(card => card.isSelected = false);
 		this.selectedCards = [];
 	}
@@ -78,5 +78,9 @@ export class BoardComponent implements OnInit {
 	swapCardOnBoard(oldCard: Card, newCard: Card) {
 		let index = this.boardCards.findIndex(card => card === oldCard);
 		this.boardCards.splice(index, 1, newCard);
+	}
+
+	checkSets() {
+		console.log(this.setCheckingService.getAvailableSets(this.boardCards, this.gameConfig.cardsPerSet));
 	}
 }
--- src/app/services/set-checking.service.ts
+++ src/app/services/set-checking.service.ts
@@ -3,19 +3,45 @@ import { Card } from '../../models/card';
 
 @Injectable()
 export class SetCheckingService {
+	count: number = 0;
 
 	constructor() { }
 
-	public isSet(cards: Card[]) {
+	public isSet(cards: Card[]): boolean {
 		let properties = ['color', 'filling', 'type', 'amount'];
-		return properties.every(prop => this.isPropertySet(cards, prop));		
+		return properties.every(prop => this.isPropertySet(cards, prop));
+	}
+
+	public getAvailableSets(cards: Card[], amountOfCardsPerSet: number): Card[][] {
+		return this.recursiveSetChecker(cards, [], amountOfCardsPerSet, 0);
+	}
+
+	public recursiveSetChecker(cards: Card[], currentCards: Card[],
+		amountOfCardsPerSet: number, startingIndex: number): Card[][] {
+		let availableSets: Card[][] = [];
+		let numOfIterations = cards.length - amountOfCardsPerSet + currentCards.length + 1;
+		for (let i = startingIndex; i < numOfIterations; i++) {
+			currentCards.push(cards[i]);
+			if (currentCards.length == amountOfCardsPerSet) {
+				if (this.isSet(currentCards)) {
+					availableSets = availableSets.concat(currentCards);
+				}
+			}
+			else {
+				this.recursiveSetChecker(cards, currentCards, amountOfCardsPerSet, i + 1);
+			}
+
+			currentCards.pop();
+		}
+
+		return availableSets;
 	}
 
 	private isPropertySet(cards: Card[], prop: string) {
 		return this.isSamePropertyValue(cards, prop) || this.isDifferrentPropertyValue(cards, prop);
 	}
 
-	private isSamePropertyValue(cards: Card[], prop: string){
+	private isSamePropertyValue(cards: Card[], prop: string) {
 		let propValue = cards[0][prop];
 		return cards.every(card => card[prop] === propValue);
 	}
